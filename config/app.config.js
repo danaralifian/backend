eaaf5ba57cd395490522a7a827cfe37b13668ed9
danar_alifian <danaralifian@gmail.com>
2020-09-08 06:29:29 +0700

    apply ignore

--- config/app.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    DB_URI : 'postgres://gwqtpzwkiigzxl:2bb07e0b80f29f88ffc0fdf5be18d5610f47de668007197d70ac3cddf7cf49e2@ec2-35-173-94-156.compute-1.amazonaws.com:5432/dql1f0eanq8cp',
-    user : 'gwqtpzwkiigzxl',
-    testDb : 'postgres://gwqtpzwkiigzxl:2bb07e0b80f29f88ffc0fdf5be18d5610f47de668007197d70ac3cddf7cf49e2@ec2-35-173-94-156.compute-1.amazonaws.com:5432/dql1f0eanq8cp',
-    host : 'ec2-35-173-94-156.compute-1.amazonaws.com',
-}
